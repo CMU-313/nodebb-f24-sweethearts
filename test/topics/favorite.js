3775c2d2a2753e5842b79d1f0b25737a11490e56
Cheyu Tu <cheyut@andrew.cmu.edu>
2024-10-10 22:35:10 -0400

    fixed test/topics/favorite.js to pass lint test

--- test/topics/favorite.js
+++ test/topics/favorite.js
@@ -6,16 +6,16 @@ const db = require('../database');
 const Topics = {};
 require('../../src/topics/favorite')(Topics); // Corrected path
 
-describe('Topics', function () {
+describe('Topics', () => {
 	let dbCalls;
 
-	beforeEach(function () {
+	beforeEach(() => {
 		dbCalls = {
 			setAdd: [],
 			sortedSetAdd: [],
 			setRemove: [],
 			sortedSetRemove: [],
-			isSortedSetMember: []
+			isSortedSetMember: [],
 		};
 
 		db.setAdd = async (...args) => { dbCalls.setAdd.push(args); };
@@ -26,58 +26,58 @@ describe('Topics', function () {
 		Topics.exists = async () => true;
 	});
 
-	describe('.toggleFavorite()', function () {
-		it('should throw an error if topic does not exist', async function () {
+	describe('.toggleFavorite()', () => {
+		it('should throw an error if topic does not exist', async () => {
 			Topics.exists = async () => false;
 			try {
 				await Topics.toggleFavorite(1, 1);
 			} catch (err) {
-                assert.strictEqual(err.message, '[[error:no-topic]]');
-            }
-        });
+				assert.strictEqual(err.message, '[[error:no-topic]]');
+			}
+		});
 
-        it('should unfavorite a topic if already favorited', async function () {
-            db.isSortedSetMember = async () => true;
-            const result = await Topics.toggleFavorite(1, 1);
-            assert.strictEqual(result, false);
-            assert.deepStrictEqual(dbCalls.setRemove, [['tid:1:favoriters', 1]]);
-            assert.deepStrictEqual(dbCalls.sortedSetRemove, [['uid:1:favorite_tids', 1]]);
-        });
+		it('should unfavorite a topic if already favorited', async () => {
+			db.isSortedSetMember = async () => true;
+			const result = await Topics.toggleFavorite(1, 1);
+			assert.strictEqual(result, false);
+			assert.deepStrictEqual(dbCalls.setRemove, [['tid:1:favoriters', 1]]);
+			assert.deepStrictEqual(dbCalls.sortedSetRemove, [['uid:1:favorite_tids', 1]]);
+		});
 
-        it('should favorite a topic if not already favorited', async function () {
-            db.isSortedSetMember = async () => false;
-            const result = await Topics.toggleFavorite(1, 1);
-            assert.strictEqual(result, true);
-            assert.deepStrictEqual(dbCalls.setAdd, [['tid:1:favoriters', 1]]);
-            assert.strictEqual(dbCalls.sortedSetAdd.length, 1);
-            assert.strictEqual(dbCalls.sortedSetAdd[0][0], 'uid:1:favorite_tids');
-            assert.strictEqual(dbCalls.sortedSetAdd[0][2], 1);
-        });
-    });
+		it('should favorite a topic if not already favorited', async () => {
+			db.isSortedSetMember = async () => false;
+			const result = await Topics.toggleFavorite(1, 1);
+			assert.strictEqual(result, true);
+			assert.deepStrictEqual(dbCalls.setAdd, [['tid:1:favoriters', 1]]);
+			assert.strictEqual(dbCalls.sortedSetAdd.length, 1);
+			assert.strictEqual(dbCalls.sortedSetAdd[0][0], 'uid:1:favorite_tids');
+			assert.strictEqual(dbCalls.sortedSetAdd[0][2], 1);
+		});
+	});
 
-    describe('.favorite()', function () {
-        it('should add a topic to favorites', async function () {
-            await Topics.favorite(1, 1);
-            assert.deepStrictEqual(dbCalls.setAdd, [['tid:1:favoriters', 1]]);
-            assert.strictEqual(dbCalls.sortedSetAdd.length, 1);
-            assert.strictEqual(dbCalls.sortedSetAdd[0][0], 'uid:1:favorite_tids');
-            assert.strictEqual(dbCalls.sortedSetAdd[0][2], 1);
-        });
-    });
+	describe('.favorite()', () => {
+		it('should add a topic to favorites', async () => {
+			await Topics.favorite(1, 1);
+			assert.deepStrictEqual(dbCalls.setAdd, [['tid:1:favoriters', 1]]);
+			assert.strictEqual(dbCalls.sortedSetAdd.length, 1);
+			assert.strictEqual(dbCalls.sortedSetAdd[0][0], 'uid:1:favorite_tids');
+			assert.strictEqual(dbCalls.sortedSetAdd[0][2], 1);
+		});
+	});
 
-    describe('.unfavorite()', function () {
-        it('should remove a topic from favorites', async function () {
-            await Topics.unfavorite(1, 1);
-            assert.deepStrictEqual(dbCalls.setRemove, [['tid:1:favoriters', 1]]);
-            assert.deepStrictEqual(dbCalls.sortedSetRemove, [['uid:1:favorite_tids', 1]]);
-        });
-    });
+	describe('.unfavorite()', () => {
+		it('should remove a topic from favorites', async () => {
+			await Topics.unfavorite(1, 1);
+			assert.deepStrictEqual(dbCalls.setRemove, [['tid:1:favoriters', 1]]);
+			assert.deepStrictEqual(dbCalls.sortedSetRemove, [['uid:1:favorite_tids', 1]]);
+		});
+	});
 
-    describe('.isFavorited()', function () {
-        it('should check if a topic is favorited', async function () {
-            const result = await Topics.isFavorited(1, 1);
-            assert.strictEqual(result, false);
-            assert.deepStrictEqual(dbCalls.isSortedSetMember, [['uid:1:favorite_tids', 1]]);
-        });
-    });
+	describe('.isFavorited()', () => {
+		it('should check if a topic is favorited', async () => {
+			const result = await Topics.isFavorited(1, 1);
+			assert.strictEqual(result, false);
+			assert.deepStrictEqual(dbCalls.isSortedSetMember, [['uid:1:favorite_tids', 1]]);
+		});
+	});
 });
