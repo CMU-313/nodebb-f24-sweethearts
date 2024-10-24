677445a011e3eec981697c71ec1683bd877487ca
jullia02 <jmontejo@andrew.cmu.edu>
2024-10-23 23:51:37 -0400

    Fixed lint errors.

--- require-main.js
+++ require-main.js
@@ -4,10 +4,10 @@
 // this allows plugins to use `require.main.require` to reference NodeBB modules
 // without worrying about multiple parent modules
 if (require.main !== module) {
-    // This makes sure that require.main is not overwritten by the module or undefined
-    if (require.main) {
-        require.main.require = function (path) {
-            return require(path);
-        };
-    }
+	// This makes sure that require.main is not overwritten by the module or undefined
+	if (require.main) {
+		require.main.require = function (path) {
+			return require(path);
+		};
+	}
 }
